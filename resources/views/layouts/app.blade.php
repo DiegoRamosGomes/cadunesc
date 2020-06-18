4f2bc75866b532701f2042afc3ea1587bca5f87c
Diego Ramos Gomes <diegoramosgomes5@gmail.com>
2020-06-18 19:09:19 -0300

    fix - upload logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,7 +45,7 @@
     @yield('style')
 </head>
 <body>
-<x-navbar></x-navbar>
+<x-nav-bar></x-nav-bar>
 @yield('content')
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
