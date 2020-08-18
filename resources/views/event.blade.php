002adcd87f9bd11f02ff5eb1150ba6178e706d5c
Diego Ramos Gomes <diegoramosgomes5@gmail.com>
2020-08-18 07:38:45 -0300

    fix - adicionado responsividade no calendario de eventos

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -2,21 +2,64 @@
 
 @section('style')
     <link href='https://cdn.jsdelivr.net/npm/fullcalendar@5.0.0/main.min.css' rel='stylesheet' />
+
+    <style>
+        #calendar {
+            width: 25%;
+        }
+
+        .event-list {
+            width: 50%;
+        }
+
+        .event-logo {
+            width: 100px;
+        }
+
+        .event-description {
+            overflow-wrap: break-word;
+            word-wrap: break-word;
+            hyphens: auto;
+            width: 75%;
+        }
+
+        .fc-toolbar {
+            font-size: .9rem;
+        }
+
+        @media only screen and (max-width: 600px) {
+            #calendar, .event-list {
+                width: 100%;
+            }
+
+            .event-logo {
+                width: 20%;
+            }
+
+            .m-sm-0 {
+                margin: 0 !important;
+            }
+
+            .event-list h4 {
+                margin: .5rem 0;
+            }
+        }
+    </style>
 @endsection
 
 @section('content')
-    <div class="d-flex justify-content-around my-5">
-        <div class="w-25" id='calendar'></div>
-        <div class="w-50">
+    <div class="d-flex flex-wrap justify-content-around my-5">
+        <div id='calendar' class="mx-2"></div>
+        <div class="event-list">
             <h4 class="text-center">Eventos de Junho</h4>
             <h3 class="text-yellow text-center">22</h3>
             <hr>
             @foreach ($events as $event)
-                <div class="d-flex my-3">
-                    <img src="{{ $event->image }}" alt="{{ $event->name }}" style="width: 100px;" width="">
-                    <div class="ml-3 justify-content-around">
-                        <p class="">{{ $event->name }}</p>
-                        <p class="small m-0">{{ \Illuminate\Support\Str::limit($event->description, 150) }}</p>
+                <div class="d-flex m-3">
+                    <img src="{{ $event->image }}" alt="{{ $event->name }}" class="event-logo">
+                    <div class="ml-3 justify-content-around event-description">
+                        <p class="m-sm-0">{{ $event->name }}</p>
+                        <p class="small m-0">{{ \Illuminate\Support\Str::limit($event->description, 100) }}</p>
                     </div>
                 </div>
             @endforeach
@@ -32,7 +75,7 @@
             let calendarEl = document.getElementById('calendar');
             let calendar = new FullCalendar.Calendar(calendarEl, {
                 initialView: 'dayGridMonth',
-                height: 500
+                height: 'auto'
             });
             calendar.render();
         });
