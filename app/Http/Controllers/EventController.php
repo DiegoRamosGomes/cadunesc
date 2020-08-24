582c817a8364f10b1eac5b69e274e338d3296dbf
Diego Ramos Gomes <diegoramosgomes5@gmail.com>
2020-08-24 19:09:53 -0300

    refactor - chaged so to linux

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 
 
 use App\Models\Event;
+use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
@@ -14,5 +15,29 @@ class EventController extends Controller
         return view('event')->with([
             'events' => $events
         ]);
+    }
+
+    public function show(Request $request)
+    {
+        return [
+            [
+                'id' => '1',
+                'name' => 'Evento 1',
+                'description' => 'Descricao do evento 1',
+                'image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR_JFyvU2eGQdS6hx7TQOloUXX3EpC_947U-jWJwF-OdfB2EAQM&usqp=CAU'
+            ],
+            [
+                'id' => '2',
+                'name' => 'Evento 2',
+                'description' => 'Descricao do evento 2',
+                'image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR_JFyvU2eGQdS6hx7TQOloUXX3EpC_947U-jWJwF-OdfB2EAQM&usqp=CAU'
+            ],
+            [
+                'id' => '3',
+                'name' => 'Evento 3',
+                'description' => 'Descricao do evento 3',
+                'image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR_JFyvU2eGQdS6hx7TQOloUXX3EpC_947U-jWJwF-OdfB2EAQM&usqp=CAU'
+            ]
+        ];
     }
 }
