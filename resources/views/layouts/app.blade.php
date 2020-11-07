827c06c608ee8605259c9ef04660488114bcc120
Diego Ramos Gomes <diegoramosgomes5@gmail.com>
2020-11-07 17:24:37 -0300

    feat - adicionado favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta http-equiv="Content-Language" content="pt-br">
+    <link rel="shortcut icon" href="{{ asset('storage/logo.ico') }}">
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Playfair+Display:400,600%7CMontserrat:400,700&amp;display=swap" media="all">
